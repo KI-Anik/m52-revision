d86c85abaef02f3150b1460d2f810f194e5da356
KI-Anik <iamkhairul101@gmail.com>
2025-01-08 14:38:34 +0600

    add NewsCard component and integrate it into CategoryNews page to display news articles

---
+++ src/components/NewsCard.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable react/prop-types */
+const NewsCard = ({ news }) => {
+  return (
+    <div className="card mb-3 bg-base-100 shadow-xl">
+      <figure>
+        <img src={news.thumbnail_url} alt="Thumbnail" className="rounded-t-xl" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title text-xl font-bold">{news.title}</h2>
+        <p className="text-sm text-gray-500">{news.details.slice(0, 100)}...</p>
+        <div className="flex items-center mt-4">
+          <div className="avatar">
+            <div className="w-12 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+              <img src={news.author.img} alt={news.author.name} />
+            </div>
+          </div>
+          <div className="ml-3">
+            <h3 className="font-semibold text-sm">{news.author.name}</h3>
+            <p className="text-xs text-gray-400">{news.author.published_date}</p>
+          </div>
+        </div>
+        <div className="card-actions justify-between items-center mt-5">
+          <div className="badge badge-outline">
+            <span className="font-medium">{news.rating.badge}</span> ({news.rating.number})
+          </div>
+          <button className="btn btn-sm btn-primary">Read More</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewsCard;
--- src/components/pages/CategoryNews.jsx
+++ src/components/pages/CategoryNews.jsx
@@ -1,12 +1,19 @@
 import { useLoaderData } from "react-router-dom";
+import NewsCard from "../NewsCard";
 
 const CategoryNews = () => {
-    const {data} = useLoaderData()
+    const {data: news} = useLoaderData()
+    console.log(news)
     return (
-        <div>
-            <h3> 
-            {data.length} News found in this category
-            </h3>
+        <div className="w-11/12 mx-auto">
+            <h2 className="font-semibold">  Dragon News Home</h2>
+            <p className="text-gray-400"> {news.length} News found in this category </p>
+            <div>
+                {
+                    news.map(singleCard =>
+                         <NewsCard key={singleCard._id} news={singleCard}></NewsCard>)
+                }
+            </div>
         </div>
     );
 };
