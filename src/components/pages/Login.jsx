13a3814cda56f3c7685757b0b5aa9fa762c3373a
KI-Anik <iamkhairul101@gmail.com>
2025-01-09 23:20:29 +0600

    implement login functionality in Login component; update AuthProvider to include logIn method for user authentication

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,36 +1,57 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../provider/AuthProvider";
+
 
 const Login = () => {
-    return (
-        <div className= "min-h-screen flex justify-center items-center ">
-             <div className="card bg-base-100 w-full max-w-lg rounded-none p-10">
-      <form className="card-body">
-        <h2 className="text-2xl font-semibold text-center">Login your account</h2>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
-        </div>
-        <div className="form-control mt-6">
-          <button className="btn btn-neutral rounded-none">Login</button>
-        </div>
-      </form>
+  const {logIn,setUser} = useContext(AuthContext)
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    const form = e.target;
+    const email = form.email.value
+    const password = form.password.value
+
+    // console.log({email, password})
+    logIn(email,password)
+    .then((result)=> {
+      console.log(result.user)
+      setUser(result.user)
+    })
+    .catch(error => {
+      alert(error.code)
+    })
+  }
+  return (
+    <div className="min-h-screen flex justify-center items-center ">
+      <div className="card bg-base-100 w-full max-w-lg rounded-none p-10">
+        <form onSubmit={handleSubmit} className="card-body">
+          <h2 className="text-2xl font-semibold text-center">Login your account</h2>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input name="email" type="email" placeholder="email" className="input input-bordered" required />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input name="password" type="password" placeholder="password" className="input input-bordered" required />
+            <label className="label">
+              <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+            </label>
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-neutral rounded-none">Login</button>
+          </div>
+        </form>
         <p className="text-center">
-            Don&apos;t have a account? <Link className="text-red-500" to={'/auth/register'}>Register</Link>
+          Don&apos;t have a account? <Link className="text-red-500" to={'/auth/register'}>Register</Link>
         </p>
+      </div>
     </div>
-        </div>
-    );
+  );
 };
 
 export default Login;
--- src/components/provider/AuthProvider.jsx
+++ src/components/provider/AuthProvider.jsx
@@ -1,37 +1,42 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react-refresh/only-export-components */
 import { createContext, useEffect, useState } from "react";
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from "../firebase/firebase.config";
 
 export const AuthContext = createContext()
 const auth = getAuth(app)
 
-const AuthProvider = ({children}) => {
-    const [user, setUser] =useState(null)
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
 
     const createNewUser = (email, password) => {
-        return createUserWithEmailAndPassword(auth,email,password)
+        return createUserWithEmailAndPassword(auth, email, password)
     }
 
-    const logOut = ()=> {
+    const logIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const logOut = () => {
         return signOut(auth)
     }
     const authInfo = {
         user,
         setUser,
         createNewUser,
+        logIn,
         logOut
     }
 
-    useEffect(()=>{
-        const unsubscribe = onAuthStateChanged(auth, currentUser=>{
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser)
         })
-        return ()=>{
+        return () => {
             unsubscribe()
         }
-    },[])
+    }, [])
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
